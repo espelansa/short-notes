ab38cca5530290604b02ba132b0d4e40a1963a38
陶莉 <li.tao@rootant.com>
2020-07-14 13:09:54 +0800

    update interface

--- TypeScript/Interface.ts
+++ TypeScript/Interface.ts
@@ -14,7 +14,7 @@ let length2: number = (value2 as string).length
 
 // Interface
 
-// 1. readonly 表示只读属性
+// 1. readonly 表示只读属性，只读属性是不允许修改的
 interface Point {
   readonly x: number;
   readonly y: number;
@@ -48,15 +48,15 @@ console.log(mySquare) // { color: 'white', area: 100 }
 // 3. 额外属性检查
 // 上述 mySquare 如果传入参数如果为 { colour: 'red' } 则会报错，原因是对象字面量赋值给变量/作为参数传递的时候会经过额外属性检查，一旦存在目标类型不包含的属性就会报错。
 
-// 绕开检查的方法一和方法二：类型断言的两种方法
+// 绕开检查的方法一：类型断言的两种方法
 let anotherSquare1 = createSquare({ colour: 'red' } as SquareConfig);
 let anotherSquare2 = createSquare((<SquareConfig>{ colour: 'red' }));
 
-// 绕开检查的方法三：增加任意数量的其他属性（最佳）
+// 绕开检查的方法二：增加任意数量的其他属性（最佳）
 interface NewSquareConfig {
   color?: string
   width?: number
-  [propName: string]: any
+  [x: string]: any
 }
 function newCreateSquare(config: NewSquareConfig): { color: string; area: number } {
   let newSquare = { color: 'white', area: 100 }
@@ -64,7 +64,7 @@ function newCreateSquare(config: NewSquareConfig): { color: string; area: number
 }
 let anotherSquare3 = newCreateSquare({ colour: 'red' })
 
-// 绕开检查的方法四：把字面量先赋值给一个变量，再用变量传参
+// 绕开检查的方法三：把字面量先赋值给一个变量，再用变量传参
 let squareOptions = { colour: "red", width: 100 };
 let anotherSquare4 = createSquare(squareOptions);
 
@@ -84,15 +84,23 @@ mySearch = function (src: string, sub: number): boolean {
 
 // 5. 可索引类型
 
-class Animal {
-  name: string;
+// 当你不确定一个接口中有多少个属性的时候就可以使用可索引类型接口
+// TypeScript支持两种索引签名：字符串和数字
+
+interface StringArray {
+  [index: number]: string
 }
-class Dog extends Animal {
-  breed: string;
+let chars: StringArray = ['a', 'b'];
+
+// 一定要注意的是两种索引签名混用的时候，数字索引签名的返回值一定要是字符串索引签名返回值的子类型，这是因为JavaScript会进行类型转换，将number转换成string
+interface Names {
+  [x: string]: string
+  // y: number 与上面一条冲突了
+  [z: number]: string
+  // [h: number]: number
 }
 
-// 错误：使用数值型的字符串索引，有时会得到完全不同的Animal!
-interface NotOkay {
-  // [x: number]: Animal;
-  [x: string]: Dog;
-}
+
+
+
+
